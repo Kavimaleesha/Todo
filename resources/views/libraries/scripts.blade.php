850a5dfdbe23fe74175689127af37df78cf4dfe6
Kavindu Maleesha <91423119+Kavimaleesha@users.noreply.github.com>
2025-04-08 15:48:48 +0530

    Initial commit

---
+++ resources/views/libraries/scripts.blade.php
@@ -0,0 +1,6 @@
+<!-- JavaScript Bundle with Popper -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-U1DAWAznBHeqEIlVSCgzq+c99gGAJns5c/t99JyeKa9xxaYpSVHU5awsuZVVFIhvj" crossorigin="anonymous">
+</script>
+
+@stack('js')
